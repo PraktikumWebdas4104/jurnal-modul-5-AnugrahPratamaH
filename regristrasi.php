af1b8e9ba0cac68c9853c05bcbfe23b45c69ca91
Anugrah Pratama H <43516625+AnugrahPratamaH@users.noreply.github.com>
2018-10-05 10:01:05 +0700

    Update regristrasi.php
    Anugrah Pratama 
    6701174034
    D3MI4104

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,45 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="number" name="nim" length="10" >
+			</td>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" length ="25"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="daftar"></td>
+		</tr>
+	</table>
+</form>
+
+<?php 
+	$host="localhost";
+	$user="root";
+	$pass="";
+	$db="db_mhs";
+
+	$conn=mysqli_connect($host,$user,$pass,$db)or die("Koneksi gagal");
+
+	if(isset($_POST['submit'])){
+		include 'prosesregris.php';
+			
+
+			/*}else{
+			echo "gabisa bro";
+			}*/
+
+		
+
+	}
 
 
-
-</body>
-</html>
+ ?>
